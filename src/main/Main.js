7520bd7ba9f9fb7bc805ecbf2c0c1386cf84735f
artyomromaniv <romaniv.artyom8@gmail.com>
2023-03-01 14:55:07 +0300

    all blocks done to mobile version

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,7 +2,6 @@ import React from "react";
 import s from'./Main.module.css';
 import sContainer from './../common/styles/Container.module.css'
 
-
 export const Main = () => {
     return (
         <div className={s.mainBlock}>
