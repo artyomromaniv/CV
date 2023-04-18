7b9e78bd22b8133453929b69a9d5e0f072912986
artyomromaniv <romaniv.artyom8@gmail.com>
2023-04-18 22:38:25 +0300

    sass

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -1,15 +1,13 @@
 import React from "react";
-import s from './Project.module.css'
+import s from './Project.module.scss'
 
 
 export const Project = (props) => {
 
     return (
         <div className={s.project}>
-            <div className={s.icon} style={props.style}>
-                <div className={s.iconClick}>
-                    <a href="#">Look</a>
-                </div>
+            <div className={s.image} style={props.style}>
+                    <a className={s.viewBtn} href="#">Look</a>
             </div>
             <div className={s.projectInfo}>
                 <h3 className={s.projectTitle}>{props.title}</h3>
